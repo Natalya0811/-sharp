fbd52c9a07f8e7abebce3d9f9c61c96a2ff617de
Natalya <natasha081196@rambler.ru>
2023-01-08 15:46:09 +0300

    Изменения в задаче2

--- Seminar1/Zadacha2/Program.cs
+++ Seminar1/Zadacha2/Program.cs
@@ -4,7 +4,7 @@
 Console.WriteLine("Введите первое число");
 int a =Convert.ToInt32 (Console.ReadLine());
 
-Console.WriteLine("Введите второе число число");
+Console.WriteLine("Введите второе число");
 int b =Convert.ToInt32 (Console.ReadLine());
 
 Console.WriteLine("Введите третье число");
