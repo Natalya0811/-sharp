5f0b034fb0405e95c73e6beb13838d0d68ebb4f6
Natalya <natasha081196@rambler.ru>
2023-01-08 20:58:23 +0300

    Задача4

--- Seminar1/Zadacha4/Program.cs
+++ Seminar1/Zadacha4/Program.cs
@@ -1,25 +1,11 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает 
 // все четные числа от 1 до N
 
-
 Console.WriteLine("Введите число");
 int n =Convert.ToInt32 (Console.ReadLine());
 
-int i =1;
-while ( i <= n )
+for(int i=1; i<=n; i++)
 {
-
-}if ( i %2 == 0)
-{
-    Console.WriteLine ( i+ " , ");
+if(i%2 == 0)
+Console.Write(i + "," );
 }
-
-i ++;
-
-{
-    Console.WriteLine ("Нет четных чисел");
-}
-
-
-    
-
