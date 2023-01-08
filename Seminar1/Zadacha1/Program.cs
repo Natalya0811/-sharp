621910ad5ce0f4cf637522ebb62d973b232cd004
Natalya <natasha081196@rambler.ru>
2023-01-08 15:41:36 +0300

    Внесли изменения по первой задаче

--- Seminar1/Zadacha1/Program.cs
+++ Seminar1/Zadacha1/Program.cs
@@ -1,5 +1,4 @@
 ﻿// Напишите программу, которая на вход принимает два числа, и выдает, какое число большее, а какое меньшее
-// a=5, b=7, max=7
 
 Console.WriteLine("Введите первое число");
 
@@ -9,5 +8,11 @@ Console.WriteLine("Введите второе число");
 
 int b =Convert.ToInt32 (Console.ReadLine());
 
-if (a>b) Console.WriteLine ($"max {a}");
-else Console.WriteLine ($"max {b}");
+if (a > b)
+{
+Console.WriteLine("Большее число " + a + " , меньшее число " + b);
+}
+else
+{
+Console.WriteLine("Большее число " + b + " , меньшее число " + a);
+}
