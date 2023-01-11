03d533411b9405ba74f137014c2f3497965579d7
Natalya <natasha081196@rambler.ru>
2023-01-11 16:11:58 +0300

    Решение задачи1

--- Seminar3/Zadanie1/Program.cs
+++ Seminar3/Zadanie1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом
+
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10000)
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+}
+else
+{
+    int a = num / 10000;
+    int b = num %10000;
+    int c = num %100;
+    int d = num %10;
+
+    if (a == d | b == c)
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
