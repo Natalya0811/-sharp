bae82084058ba033ca8f9a9d4895d02ed0001664
Natalya <natasha081196@rambler.ru>
2023-01-12 22:16:22 +0300

    Промежуточное решение задачи1

---
+++ Seminar4/Z1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход два числа (A и B), 
+// и возводит число A в натуральную степень В
+
+Console.WriteLine("Введите первое число");
+int a =Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b =Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ($"Первое число в натуральной степени второго равно {S(a,b)}");
+
+int S(int a, int b)
+
+{
+    int result=1;
+    for ( int i=1; i < b ; i++)
+    result = result*a;
+}
+return S;
