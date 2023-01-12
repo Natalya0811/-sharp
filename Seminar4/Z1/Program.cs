c9b29ad8e718ea901f9e17c108ad6aafdd16d4c2
Natalya <natasha081196@rambler.ru>
2023-01-13 01:07:37 +0300

    Решение задачи1

--- Seminar4/Z1/Program.cs
+++ Seminar4/Z1/Program.cs
@@ -2,16 +2,16 @@
 // и возводит число A в натуральную степень В
 
 Console.WriteLine("Введите первое число");
-int a =Convert.ToInt32 (Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
-int b =Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine ($"Первое число в натуральной степени второго равно {S(a,b)}");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Первое число в натуральной степени второго равно {S(a, b)}");
 
 int S(int a, int b)
 
 {
-    int result=1;
-    for ( int i=1; i < b ; i++)
-    result = result*a;
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+        result = result * a;
+    return result;
 }
-return S;
