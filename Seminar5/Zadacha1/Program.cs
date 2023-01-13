fee4a17a08b60e27a8484720f5616d90915d11c4
Natalya <natasha081196@rambler.ru>
2023-01-14 02:33:37 +0300

    Решение задачи1

---
+++ Seminar5/Zadacha1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив заполненный случайными положительными трехзначными числами
+// Напишите программу, которая покажет количество четных чисел в массиве
+
+
+Console.WriteLine("Введите количество элементов в массиве");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ", ");
+}
+Console.WriteLine();
+
+
+void Result(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0) count++;
+    Console.WriteLine($"Количество чётных чисел в массиве равно {count}");
+}
+
+
+int[] array = new int[a];
+FillArray(array);
+PrintArray(array);
+Result(array);
