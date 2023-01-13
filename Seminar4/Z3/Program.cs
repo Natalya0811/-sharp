a82489892aaa10d6e0a726c297694adc74f9973f
Natalya <natasha081196@rambler.ru>
2023-01-13 03:25:05 +0300

    Решение задачи3

---
+++ Seminar4/Z3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задает массив из 8 целых чисел
+// с клавиатуры, и далее выводит массив на экран в одну строку
+
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[a];
+
+for (int i = 0; i < a; i++)
+{
+    array[i] = new Random().Next(a + 1);
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine();
