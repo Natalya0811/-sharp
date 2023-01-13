3bbd883f9b278dbaa4cdad1737d16eda0318ff8b
Natalya <natasha081196@rambler.ru>
2023-01-13 03:08:13 +0300

    Решение задачи2

---
+++ Seminar4/Z2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число,
+// и выдает сумму цифр в числе
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе равна {Result(a)}");
+
+
+int Result(int a)
+
+{
+    int sum = 0;
+    while (a > 0)
+    {
+        sum = sum + a % 10;
+        a = a / 10;
+    }
+    return sum;
+}
