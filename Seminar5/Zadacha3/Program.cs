20bd15134010f1455b094a68f407c5b26237240f
Natalya <natasha081196@rambler.ru>
2023-01-15 03:37:32 +0300

    Решение задачи3

--- Seminar5/Zadacha3/Program.cs
+++ Seminar5/Zadacha3/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Задайте массив вещественных чисел
 // Найдите разницу между максимальным и минимальным элементом массива
 
-
 Console.WriteLine("Введите количество элементов в массиве");
 int a = Convert.ToInt32(Console.ReadLine());
 
@@ -19,39 +18,22 @@ void PrintArray(double[] array)
 }
 Console.WriteLine();
 
-void Maximum(double[] array)
-{
-    double max = array [0];
-    for (int i = 0; i < array.Length; i++)
-    if (array[i] > max)
-    {
-        max = array[i];
-       }
- Console.WriteLine($"максимальный элемент в массиве {max}");
-}
 
-void Minimum(double[] array)
+
+void Result(double[] array)
 {
-    double min = array [0];
+    double max = array[0];
+    double min = array[0];
     for (int i = 0; i < array.Length; i++)
-    if (array[i] < min)
     {
-        min = array[i];
-       
+        if (array[i] > max) max = array[i];
+        if (array[i] < min) min = array[i];
     }
- Console.WriteLine($"минимальный элемент в массиве {min}");
+    Console.WriteLine($"Разница между максимальным и минимальным элементом массива равна {max - min}");
 }
-
-void Result(double [] array)
-{
-Console.WriteLine($"{Maximum} - {Minimum} = {Result}");
-}
-
 
 
 double[] array = new double[a];
 FillArray(array);
 PrintArray(array);
-Maximum(array);
-Minimum(array);
 Result(array);
