1893d692a60777ae50a29cce42970f87a1141560
Natalya <natasha081196@rambler.ru>
2023-01-16 19:15:59 +0300

    Решение задачи1

--- Seminar6/Zadanie1/Program.cs
+++ Seminar6/Zadanie1/Program.cs
@@ -2,5 +2,33 @@
 // Посчитайте, сколько чисел больше 0 ввел пользователь
 
 
+Console.WriteLine("Введите количество чисел M");
+int m = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите последовательность чисел через пробел");
+
+void FillArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i+1} число");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    
+}
+
+int Result(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count+=1;
+    }
+    return count;
+    
+}
+
+
+int[] array = new int[m];
+FillArray(array);
+Result(array);
+Console.WriteLine($"Количество чисел больше 0 : {Result(array)}");
