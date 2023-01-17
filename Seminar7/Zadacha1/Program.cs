684ecab267aabce62a311b0fde5c69393d0ab3d4
Natalya <natasha081196@rambler.ru>
2023-01-18 01:44:25 +0300

    Решение задач Семинар7

---
+++ Seminar7/Zadacha1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив размером m на n, заполненный случайными вещественными числами
+
+
+Console.WriteLine("Введите количество строк m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+void FillArray(double[,] array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+       for (int j=0; j<array.GetLength(1); j++)
+       {
+         array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+       }
+    }
+}
+
+
+void PrintArray(double[,] array)
+{
+     for (int i=0; i<array.GetLength(0); i++)
+     {
+           for (int j=0; j<array.GetLength(1); j++)
+           Console.Write($"{array[i,j]} \t");
+           Console.WriteLine();
+     }
+}
+
+
+
+double [,] array = new double[m,n];
+FillArray(array);
+PrintArray(array);
