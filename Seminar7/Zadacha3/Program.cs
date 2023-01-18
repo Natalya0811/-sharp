eb4b639b77eacd3b7a556a83fcbc0b12e4cc0376
Natalya <natasha081196@rambler.ru>
2023-01-18 16:40:08 +0300

    прпомежуточное решение задачи3

---
+++ Seminar7/Zadacha3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив из целых чисел
+// Найдите среднее арифметическое элементов в каждом столбце
+
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 10);
+
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+void Result(double[,] array)
+{
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double Sr = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+           double res = (Sr + array[i, j]);
+        
+        res = res / 3;
+        Console.Write(res + "; ");
+        }
+        Console.WriteLine();
+
+    }
+}
+
+
+
+double[,] array = new double[3, 5];
+FillArray(array);
+PrintArray(array);
+Result(array);
+
