13a31d29d0c9d937c24899578b537bd0174b65fd
Natalya <natasha081196@rambler.ru>
2023-01-20 23:24:03 +0300

    Решение задачи5

---
+++ Seminar8/Задача5/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4
+
+Console.WriteLine("Введите размер массива");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[a, a];
+
+int n= 1;
+int i = 0;
+int j = 0;
+
+while (n <= a * a)
+{
+    array[i, j] = n;
+    if (i <= j + 1 && i + j < a - 1)
+        ++j;
+    else if (i < j && i + j >= a - 1)
+        ++i;
+    else if (i >= j && i + j > a - 1)
+        --j;
+    else
+        --i;
+    ++n;
+}
+
+PrintArray(array);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(" ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("");
+        Console.WriteLine("");
+    }
+}
