7334ba3aa3dce9ef66d163796412a889d4999567
Natalya <natasha081196@rambler.ru>
2023-01-20 03:19:16 +0300

    Решение задачи3

---
+++ Seminar8/Задача3/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить 
+// произведение двух матриц
+
+int[,] array = new int[2, 2];
+int[,] secondArray = new int[2, 2];
+int[,] resultArray = new int[2, 2];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+FillArray(secondArray);
+PrintArray(secondArray);
+Console.WriteLine($"Итоговый массив после произведения");
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 10);
+
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void Result(int[,] array, int[,] secondArray, int[,] resultArray)
+
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < secondArray.GetLength(1); j++)
+        {
+            resultArray[i, j] = 0;
+            for (int n = 0; n < array.GetLength(1); n++)
+            {
+                resultArray[i, j] += array[i, n] * secondArray[n, j];
+            }
+
+        }
+    }
+}
+
+
+Result(array, secondArray, resultArray);
+PrintArray(resultArray);
+
+
