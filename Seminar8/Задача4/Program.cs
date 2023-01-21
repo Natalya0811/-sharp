1fc85c95dc65292b50cbab353eb11f9c240140c3
Natalya <natasha081196@rambler.ru>
2023-01-21 15:37:32 +0300

    Решение задачи4

--- Seminar8/Задача4/Program.cs
+++ Seminar8/Задача4/Program.cs
@@ -1,25 +1,45 @@
 ﻿// Сформируйте трехмерный массив из уникальных двузначных чисел. 
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента
 
-
 int[,,] array3D = new int[2, 2, 2];
 FillArray(array3D);
 PrintResult(array3D);
 
-
-void FillArray(int[,,] array)
+void FillArray(int[,,] array3D)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-        for (int j = 0; j < array.GetLength(1); j++)
-          for (int n = 0; n < array3D.GetLength(2); n++)
-            array[i, j, n] = new Random().Next(10, 100);
-
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < array3D.GetLength(0); x++)
+  {
+    for (int y = 0; y < array3D.GetLength(1); y++)
+    {
+      for (int z = 0; z < array3D.GetLength(2); z++)
+      {
+        array3D[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
 }
-
-
-
-
-
 
 void PrintResult(int[,,] array)
 {
