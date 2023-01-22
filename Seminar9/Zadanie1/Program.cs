0aeb648a13e69d7f118897b7e626d09b3f5eaac9
Natalya <natasha081196@rambler.ru>
2023-01-22 13:36:22 +0300

    Решение задачи1

---
+++ Seminar9/Zadanie1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1.
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+Result(n, count);
+
+
+void Result(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        Result(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
