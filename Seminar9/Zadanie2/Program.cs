f045bd9462a5196bfcd70af7d8fceff1dce7b3d4
Natalya <natasha081196@rambler.ru>
2023-01-22 13:51:15 +0300

    Решение задачи2

---
+++ Seminar9/Zadanie2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая которая найдет сумму 
+// натуральных элементов в промежутке от M до N.
+
+
+Console.Write("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Result(m, n);
+
+void Result(int m, int n)
+{
+    Console.WriteLine(Sum(m - 1, n));
+}
+
+
+int Sum(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + Sum(m, n);
+        return res;
+    }
+}
